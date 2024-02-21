dad29b50a07fcce1e98e38dec3b284174ce3c316
justobello <justobello352@gmail,com>
2024-02-21 07:30:51 -0400

    pequeño cambio

--- src/Pages/Lista.jsx
+++ src/Pages/Lista.jsx
@@ -14,7 +14,7 @@ function Lista({ imagen, cantante, apodo }) {
                 <p> {apodo} </p>
             </div>
             <div>
-                <button>Reproducir</button>
+                <button>Repro</button>
             </div>
 
 
